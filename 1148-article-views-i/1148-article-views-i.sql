e336816dfcf876ca51f75b223ea99612c8220d3b
Anilia Anil <114533243+anilia-a@users.noreply.github.com>
2024-04-22 19:41:08 +0100

    Time: 823 ms (50.36%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 SELECT DISTINCT author_id id FROM Views
-WHERE (author_id = viewer_id) >= 1 ORDER BY author_id ASC;
+WHERE author_id = viewer_id ORDER BY author_id;
